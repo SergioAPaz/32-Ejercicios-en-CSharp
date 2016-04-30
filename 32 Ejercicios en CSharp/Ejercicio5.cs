8cedbe3ef3ea784d65bcfb7d84fd35efe6092c4c
SergioAPaz <alejandroax@live.com.mx>
2016-04-30 17:27:58 -0600

    5-6

---
+++ 32 Ejercicios en CSharp/Ejercicio5.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _32_Ejercicios_en_CSharp
+{
+    class Ejercicio5
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("imprimir los numeros impares hasta el cien e imprimir cuantos impares hay: \n");
+            int Contador = 0;
+            for (int i = 1; i <= 100; i=i+2)
+            {
+                Contador = Contador+1;
+                Console.WriteLine(i);
+
+            }
+            Console.WriteLine("\nEl numero de impares que hay es: {0}",Contador);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 32 Ejercicios en CSharp/Ejercicio6.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _32_Ejercicios_en_CSharp
+{
+    class Ejercicio6
+    {
+        static void Main(string[] args)
+        {
+            String Respuesta = "s";
+            Console.WriteLine("imprimir los numeros naturales que hay desde la unidad hasta un numero que introducimos por teclado \n");
+            while (Respuesta=="s")
+            {
+                Console.WriteLine("\nFavor de ingresar un numero: ");
+                String Teclado = Console.ReadLine();
+                Console.WriteLine("\n");
+                int x = Int32.Parse(Teclado);
+                int Contador = 0;
+                for (int i = 0; i < x; i++)
+                {
+                    Contador = Contador + 1;
+                    Console.WriteLine(Contador);
+                }
+                Console.WriteLine("\nDesea seguir ingresando numeros: (s/n)?: ");
+                Respuesta = Console.ReadLine();
+               
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
