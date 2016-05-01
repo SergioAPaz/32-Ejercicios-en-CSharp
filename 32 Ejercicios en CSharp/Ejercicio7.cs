44756a24faf55f18759e4b08883976f7ab73c5b6
SergioAPaz <alejandroax@live.com.mx>
2016-04-30 18:20:37 -0600

    7-8

---
+++ 32 Ejercicios en CSharp/Ejercicio7.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _32_Ejercicios_en_CSharp
+{
+    class Ejercicio7
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Intrducir tantas frases como queramos y contarlas: \n");
+            String Respuesta = "s";
+            int Contador=0;
+            while (Respuesta=="s")
+            {
+                Contador=Contador+1;
+                Console.WriteLine("\nIntroduce una frase: ");
+                Console.ReadLine();
+                Console.WriteLine("\nDeseas seguir agregando palabras (s/n)?:");
+                Respuesta = Console.ReadLine();
+
+
+            }
+
+            Console.WriteLine("\nEl numero de palabras agregadas fue: {0}",Contador);
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ 32 Ejercicios en CSharp/Ejercicio8.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _32_Ejercicios_en_CSharp
+{
+    class Ejercicio8
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Codigo que solo permite introducir las palabras SI o NO\n");
+
+            String Respuesta="SI";
+            String Respuesta2 = "s";
+            while ((Respuesta2=="s"))
+            {
+                Console.WriteLine("\nIngresa SI o NO: ");
+                Respuesta = Console.ReadLine();
+                if ((Respuesta=="SI")||(Respuesta=="NO"))
+                {
+                    Console.WriteLine("\nBien echo sigue tecleando...\n");
+                }
+                else
+                {
+                    Console.WriteLine("Mal, solo puedes agregar SI o NO....\n");
+                    
+                }
+                Console.WriteLine("Deseas seguir en el juego (s/n)?:");
+                Respuesta2 = Console.ReadLine();
+            }
+        }
+    }
+}
