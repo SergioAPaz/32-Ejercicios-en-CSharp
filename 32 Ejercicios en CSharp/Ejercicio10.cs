916b65b209244f1fee8175f7c7fe564aa5149aba
SergioAPaz <alejandroax@live.com.mx>
2016-05-01 12:38:33 -0600

    9-11

---
+++ 32 Ejercicios en CSharp/Ejercicio10.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _32_Ejercicios_en_CSharp
+{
+    class Ejercicio10
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introducir un numero por teclado y que nos diga si es paro impar: \n");
+
+            String Respuesta = "s";
+
+            while (Respuesta=="s")
+            {
+                Console.WriteLine("\nIntroduce un numero:");
+                String Numero = Console.ReadLine();
+                Double Numero2 = Convert.ToDouble(Numero);
+
+                if (Numero2 % 2 == 0)
+                {
+                    Console.WriteLine("\nEl numero {0} es par. \n",Numero2);
+
+                }else if(Numero2%0 != 2)
+                {
+                    Console.WriteLine("\nEl numero {0} es impar.\n",Numero2);
+                }
+
+                Console.WriteLine("Deseas seguir en el juego (s/n)? ");
+                Respuesta = Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ 32 Ejercicios en CSharp/Ejercicio9.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _32_Ejercicios_en_CSharp
+{
+    class Ejercicio9
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introducir un numero y que el programa nos diga si es positivo o negativo");
+
+            string Respuesta = "s";
+
+            while (Respuesta=="s")
+            {
+                Console.WriteLine("\nTeclea un numero: ");
+                string numero = Console.ReadLine();
+
+                Double numero2=Convert.ToDouble(numero);
+
+                if (numero2>0)
+                {
+                    Console.WriteLine("\nEl numero es positivo.");
+                }
+                else
+                {
+                    Console.WriteLine("\nEl numero es negativo.");
+                }
+
+                Console.WriteLine("\nDeseas seguir en el juego (s/n)?: ");
+                Respuesta = Console.ReadLine();
+
+            }
+        }
+    }
+}
