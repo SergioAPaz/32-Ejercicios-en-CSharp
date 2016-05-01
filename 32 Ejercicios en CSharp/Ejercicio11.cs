04a16094eaf640d30850dc61cd9ad73e58c3cd44
SergioAPaz <alejandroax@live.com.mx>
2016-05-01 12:38:42 -0600

    11

---
+++ 32 Ejercicios en CSharp/Ejercicio11.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _32_Ejercicios_en_CSharp
+{
+    class Ejercicio11
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Imprimir y contar los multiplos de 3 desde la unidad hasta un numero que intorucimos por teclado: \n");
+
+            String Respuesta = "s";
+
+            while (Respuesta=="s")
+            {
+                Console.WriteLine("\nIntroduce un numero: ");
+                String Numero = Console.ReadLine();
+                Double numero2 = Convert.ToDouble(Numero);
+
+                for (int i = 0; i < numero2; i++)
+                {
+
+                    if (i%3==0 && i!=0)
+                    {
+                        Console.WriteLine("El numero {0} es multiplo de 3.",i);
+                    }
+                    else
+                    {
+                        Console.WriteLine("No hay ningun numero dentro del rango que sea multiplo de 3.");
+                    }
+                }
+
+
+                Console.WriteLine("\nDeseas seguir en el juego (s/n) ?:");
+                Respuesta = Console.ReadLine();
+
+
+            }
+        }
+    }
+}
