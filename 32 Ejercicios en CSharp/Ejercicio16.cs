3be559f345ca4662054299ed4a260b88389e65cb
SergioAPaz <alejandroax@live.com.mx>
2016-08-13 19:25:03 -0600

    Ejercicio 16

---
+++ 32 Ejercicios en CSharp/Ejercicio16.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _32_Ejercicios_en_CSharp
+{
+    class Ejercicio16
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Imprimir diez veces la serie de numeros del 1 al 10.");
+
+            int Cont = 0;
+            int Serie = 0;
+            while (Cont<=9)
+            {
+                
+                for (int i = 1; i <=10 ; i++)
+                {
+                    Console.WriteLine("\n Serie {0}",Serie);
+                    Console.WriteLine("Numero: {0}", i);
+                }
+                Serie++;
+                Cont++;
+            }
+            Console.ReadKey();
+        }
+    }
+}
